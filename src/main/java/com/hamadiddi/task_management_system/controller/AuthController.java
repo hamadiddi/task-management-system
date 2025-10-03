ee0b582571bb8e027d601402d617bf12d4538c11
Hamadi Iddi <hamaditaufik@gmail.com>
2025-10-03 10:05:28 +0300

    feat(backend): Add AuthController for register and login endpoints

--- src/main/java/com/hamadiddi/task_management_system/controller/AuthController.java
+++ src/main/java/com/hamadiddi/task_management_system/controller/AuthController.java
@@ -1,1 +1,27 @@
 package com.hamadiddi.task_management_system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.hamadiddi.task_management_system.entity.User;
+import com.hamadiddi.task_management_system.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPasswordHash(request.getPassword());
+        return authService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request.getUsername(), request.getPassword());
+    }
+}
