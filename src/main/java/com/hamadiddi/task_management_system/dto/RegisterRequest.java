0a44beb0d6a03dedce28bceb026b56b53fc65968
Hamadi Iddi <hamaditaufik@gmail.com>
2025-10-03 10:29:03 +0300

    feat(backend): Add DTOs for tasks, login, and register

--- src/main/java/com/hamadiddi/task_management_system/dto/RegisterRequest.java
+++ src/main/java/com/hamadiddi/task_management_system/dto/RegisterRequest.java
@@ -1,1 +1,10 @@
 package com.hamadiddi.task_management_system.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+}
--- src/main/java/com/hamadiddi/task_management_system/dto/TaskDto.java
+++ src/main/java/com/hamadiddi/task_management_system/dto/TaskDto.java
@@ -1,1 +1,14 @@
 package com.hamadiddi.task_management_system.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class TaskDto {
+    private Long taskId;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private LocalDateTime dueDate;
+}
